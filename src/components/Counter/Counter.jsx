965bb8f617f29cdd4bba45cc59f6375ad5df01da
M <marilynduf@gmail.com>
2025-12-16 17:29:15 -0500

    refactor: optimisation of performance with bette component structure

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -23,7 +23,7 @@ function isPrime(number) {
     return true;
 }
 
-const Counter = memo(function Counter({ initialCount }) {
+export default function Counter({ initialCount }) {
     log("<Counter /> rendered", 1);
     const initialCountIsPrime = isPrime(initialCount);
 
@@ -55,6 +55,4 @@ const Counter = memo(function Counter({ initialCount }) {
             </p>
         </section>
     );
-});
-
-export default Counter;
+}
